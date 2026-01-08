a1b7ab7bee837c3dddc662935eb5301ca364b1b1
Karl Hartinger <karl@hartingers.at>
2026-01-08 20:11:54 +0100

    Add software/rcc_module11

---
+++ software/rcc_module11_V1/src/rcc_module11_text.h
@@ -0,0 +1,91 @@
+//_____rcc_module11_text.h_______________________khartinger_____
+// define text in English and German
+#ifndef rcc_module11_TEXT_H
+ #define rcc_module11_TEXT_H
+ #include <Arduino.h>                  // String, int32_t
+ //-----------(index of) connection states----------------------
+ #define CON_UNKNOWN         0
+ #define CON_CONNECTING      1
+ #define CON_NO_WIFI         2
+ #define CON_NO_MQTT         3
+ #define CON_WIFI_OK         4
+ #define CON_MQTT_OK         5
+ #define CON_WIFI_NOT_USED   6
+
+ //----status defines as string numbers-------------------------
+ #define T1_UNDEF0           "-2"
+ #define T1_UNDEF1           "-1"
+ #define T1_STRIGHT          "1"
+ #define T1_CURVED           "0"
+ #define T1_UNKNOWN          "-9"
+ #define T1_ON               "1"
+ #define T1_OFF              "0"
+ #define T1_NO_MQTT          "-8"
+ #define T1_DPDT_0_NC        "13"
+ #define T1_DPDT_1_NO        "15"
+
+ #if LANGUAGE == 'd'
+  // =========Deutsche Texte (German)===========================
+  #define T_UNDEF0           "undefiniert_0?"
+  #define T_UNDEF1           "undefiniert_1?"
+  #define T_STRIGHT          "Gerade"
+  #define T_CURVED           "Abzweig"
+  #define T_UNKNOWN          "unmoeglich"
+  #define T_ON               "Ein"
+  #define T_OFF              "Aus"
+  #define T_NO_MQTT          "Keine MQTT Steuerung!"
+  const    String sConn[]={"-unknown--", "verbinden_", 
+         "-No WiFi--", "-No MQTT--", "WiFi OK   ", "MQTT OK   ",
+        "Ohne WiFi "};
+
+// "---------|---------|-" line length = 21
+#define  INFOLINES_NUM     10
+  #define  INFOLINES { \
+   "Modul 11:            ", \
+   "     Abzweigung      ", \
+   "     Tankstelle      ", \
+   VERSION_99_1, \
+   " Weiter: Taste IO19  ", \
+   "DCC 111: 2-Wegweiche ", \
+   "DCC 112: Fahrstrom   ", \
+   "DCC 113: Entkuppler  ", \
+   "                     ", \
+   " Weiter: Taste IO19  ", \
+  }
+
+ #else
+
+  //==========English texts=====================================
+  #ifndef LANGUAGE
+   #define LANGUAGE          'e'
+  #endif
+  #define T_UNDEF0           "undefined_0?"
+  #define T_UNDEF1           "undefined_1?"
+  #define T_STRIGHT          "stright"
+  #define T_CURVED           "curved"
+  #define T_UNKNOWN          "impossible"
+  #define T_ON               "On"
+  #define T_OFF              "Off"
+  #define T_NO_MQTT          "*No control via MQTT*"
+  const    String sConn[]={"-unknown--", "connecting", 
+         "-No WiFi--", "-No MQTT--", "WiFi OK   ", "MQTT OK   ",
+        "Unused WiFi"};
+
+// "---------|---------|-" line length = 21
+#define  INFOLINES_NUM     10
+#define  INFOLINES { \
+  "Module 11:           ", \
+  "      Turnoff        ", \
+  "   petrol station    ", \
+  VERSION_99_1, \
+  " Next: Button IO19   ", \
+  "DCC 111 2-Way-Turnout", \
+  "DCC 112  Discon Track", \
+  "DCC 112: Uncoupler   ", \
+  "DCC 113: Entkuppler  ", \
+  "                     ", \
+  " Next: Button IO19   ", \
+}
+
+ #endif
+#endif
---
+++ software/rcc_module11_V1/src/src/statemachine/D1_class_Statemachine.h
@@ -0,0 +1,66 @@
+﻿//_____D1_class_Statemachine.h________________181002-240220_____
+// The class Statemachine helps to build a state counter.
+// It counts from 1 to stateMax (incl.) and waits
+// "stateDelay" milliseconds for every state.
+// Created by Karl Hartinger, October 02, 2018.
+// Modified
+// 2018-10-05 some set/get added
+// 2018-10-19 stateMin, add() added
+// 2020-01-19 getBeginMillis() added
+// 2020-02-25 getStateMin() added
+// 2020-03-03 add(..,..), diff() added
+// 2020-04-05 add getBeginMillis()
+// 2021-01-31 state: change int to int32_t
+// 2021-04-11 add isDelayed(), delayed (ms)
+// 2024-02-20 replace delay() by while(), add getDelayed()
+// 2024-02-22 defines STATE_MINIMUM, STATE_NOW, STATE_NONE
+// Released into the public domain.
+#ifndef D1_CLASS_STATEMACHINE_H
+#define D1_CLASS_STATEMACHINE_H
+#include "Arduino.h"
+#define STATE_ONE                1     // 1st value of state
+#define STATE_LAST              10     // last value of states
+#define STATE_DELAY_DEFAULT    200     // state delay in ms
+#define STATE_MINIMUM  -2147483644
+#define STATE_NOW      -2147483645
+#define STATE_NONE     -2147483646
+
+class Statemachine {
+ protected:
+  //-----properties---------------------------------------------
+  int32_t  stateCounter;               // counts from min to max
+  int32_t  stateMin;                   // 1st counter value
+  int32_t  stateMax;                   // last vounter value
+  uint32_t stateDelay;                 // delay for one state
+  uint32_t millisBegin;                // start time of state
+  uint32_t delayed;                    // state(s) delayed in ms
+ public:
+  //-----constructor & co---------------------------------------
+  Statemachine();                      // use default values
+  Statemachine(int32_t state_max, int32_t state_delay);
+  Statemachine(int32_t state_min, int32_t state_max, int32_t state_delay);
+ protected:
+  void setup();                        // setup properties
+ public:
+  //-----set values---------------------------------------------
+  bool setStateMin(int32_t state_min); //
+  bool setStateMax(int32_t state_max); //
+  bool setStateDelay(int32_t state_delay); //
+  bool setState(int32_t new_state);    //
+  //-----get values---------------------------------------------
+  int32_t  getStateMin();              //
+  int32_t  getStateMax();              //
+  int32_t  getStateDelay();            //
+  int32_t  getState();                 //
+  int32_t  getDuration();              //
+  uint32_t getDelayed();               // state(s) delayed in ms
+  uint32_t getBeginMillis();           //
+  //-----working methods----------------------------------------
+  int32_t  loopBegin();                //
+  uint32_t loopEnd();                  //
+  int32_t  add(int32_t numberOfStates);// return(stateCounter+num)
+  int32_t  add(int32_t state, int32_t numberOfStates); // return(state+num)
+  int32_t  diff(int32_t oldState);     // return(stateCounter-old)
+  bool     isDelayed();                //
+};
+#endif
